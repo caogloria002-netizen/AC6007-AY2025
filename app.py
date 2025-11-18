8502d1da1c3f7eae9b8c965da5025252a95559af
caogloria002-netizen <caogloria002@gmail.com>
2025-11-18 03:20:44 +0000

    1

--- app.py
+++ app.py
@@ -13,6 +13,10 @@ def main():
     r = request.form.get("q")
     return(render_template("main.html"))
 
+@app.route("/dbs", methods=["GET","POST"])
+def dbs():
+    return(render_template("dbs.html"))
+
 if __name__ == "__main__":
     app.run()
     
